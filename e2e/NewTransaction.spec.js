bf450de087dfe164a1a924f01abdcfc5109ea113
piotrk39 <piotrk39@gmail.com>
2022-10-26 12:23:53 +0300

    Add Navigate, Pay and Request transaction

--- e2e/NewTransaction.spec.js
+++ e2e/NewTransaction.spec.js
@@ -1,4 +1,5 @@
 import sample from '../sampleData/loginLogoutData';
+import sampleTransaction from '../sampleData/TransactionData';
 const { test, expect } = require('@playwright/test');
 
 test.describe("User Sign-up and Login", () => {
@@ -8,7 +9,50 @@ test.describe("User Sign-up and Login", () => {
     await data.navigate();
   });
 
-    test("main navigation", async ({ page }) => {
+    test("New Transaction", async ({ page }) => {
+        // Assertions use the expect API.
+        const data = new sample(page);
+        const transaction = new sampleTransaction(page);
+        await expect(page).toHaveURL("http://localhost:3000/signin");
+
+        await data.logIn();
+        await transaction.navigateNewTransaction();
+
+    });
+
+    test("navigates to the new transaction form, selects a user and submits a transaction payment", async ({ page }) => {
+        // Assertions use the expect API.
+        const data = new sample(page);
+        const transaction = new sampleTransaction(page);
+        await expect(page).toHaveURL("http://localhost:3000/signin");
+
+        await data.logIn();
+        await transaction.navigateNewTransaction();
+        await transaction.createNewPayTransaction();
+    });
+
+    test("navigates to the new transaction form, selects a user and submits a transaction request", async ({ page }) => {
+        // Assertions use the expect API.
+        const data = new sample(page);
+        const transaction = new sampleTransaction(page);
+        await expect(page).toHaveURL("http://localhost:3000/signin");
+
+        await data.logIn();
+        await transaction.navigateNewTransaction();
+        await transaction.createNewPayTransaction();
+    });
+
+    test("displays new transaction errors", async ({ page }) => {
+        // Assertions use the expect API.
+        await expect(page).toHaveURL("http://localhost:3000/signin");
+    });
+
+    test("submits a transaction payment and verifies the deposit for the receiver", async ({ page }) => {
+        // Assertions use the expect API.
+        await expect(page).toHaveURL("http://localhost:3000/signin");
+    });
+
+    test("submits a transaction request and accepts the request for the receiver", async ({ page }) => {
         // Assertions use the expect API.
         await expect(page).toHaveURL("http://localhost:3000/signin");
     });
---
+++ sampleData/TransactionData.js
@@ -0,0 +1,68 @@
+const { expect } = require('@playwright/test');
+import sample from './loginLogoutData';
+import sampleBank from './BankAccountData';
+
+const transactionInfo = {
+    amountPay: "35",
+    notePay: "I pay you pay we all pay",
+    amountReq: "120",
+    noteReq: "Sushi was great payback is better",
+    searchPartialPay: "home",
+    searchPartialReq: "Beck",
+};
+
+export default class sampleTransaction {
+
+    //Banks data
+    constructor(page) {
+      //imported locators
+      const data = new sample(page);
+      this.bankNameField = data.bankName;
+      //New Transaction Locators
+      this.newTransactionButton = page.locator('[data-test="nav-top-new-transaction"]');
+      this.newTransactioHeader = page.getByText('Select Contact');
+      this.searchRecipientInput = page.locator('[data-test="user-list-search-input"]');
+      this.kaylinHomenickContact = page.getByText('Kaylin Homenick');
+      this.devon = page.getByText('Devon Becker');
+      this.amount = page.getByPlaceholder('Amount');
+      this.note = page.getByPlaceholder('Add a note');
+      this.pay = page.locator('[data-test="transaction-create-submit-payment"]');
+      this.request = page.locator('[data-test="transaction-create-submit-request"]');
+      this.confirmation = page.locator('div:nth-child(2) > div:nth-child(2) > div');
+      this.returnToTransactions = page.locator('[data-test="new-transaction-return-to-transactions"]');
+      this.myTransactions = page.locator('[data-test="nav-personal-tab"]');
+      this.transactionPaid = page.getByRole('grid', { name: 'grid' }).locator('div:has-text("Bob Ross paid Kaylin HomenickI pay you pay we all pay00-$35.00")').nth(1).first();
+      this.transactionReq = page.getByRole('grid', { name: 'grid' }).locator('div:has-text("Bob Ross requested Devon BeckerSushi was great payback is better00+$120.00")').nth(1).first();
+    }
+
+    async navigateNewTransaction() {
+        await this.newTransactionButton.click();
+        await expect(this.newTransactioHeader).toHaveText('Select Contact');
+    }
+
+    async createNewPayTransaction() {
+      await this.searchRecipientInput.type(transactionInfo.searchPartialPay);
+      await expect(this.kaylinHomenickContact).toHaveText('Kaylin Homenick');
+      await this.kaylinHomenickContact.click();
+      await this.amount.type(transactionInfo.amountPay);
+      await this.note.type(transactionInfo.notePay);
+      await this.pay.click();
+      await expect(this.confirmation).toHaveText('Paid $35.00 for I pay you pay we all pay')
+      await this.returnToTransactions.click();
+      await this.myTransactions.click();
+      await expect(this.transactionPaid).toHaveText('Bob Ross paid Kaylin HomenickI pay you pay we all pay00-$35.00');
+    }
+
+    async createNewRequestTransaction() {
+        await this.searchRecipientInput.type(transactionInfo.searchPartialReq);
+        await expect(this.devon).toHaveText('Devon Becker');
+        await this.devon.click();
+        await this.amount.type(transactionInfo.amountReq);
+        await this.note.type(transactionInfo.noteReq);
+        await this.request.click();
+        await expect(this.confirmation).toHaveText('Requested $120.00 for Sushi was great payback is better')
+        await this.returnToTransactions.click();
+        await this.myTransactions.click();
+        await expect(this.transactionReq).toHaveText('Bob Ross requested Devon BeckerSushi was great payback is better00+$120.00');
+      }
+}
